ba2e52e071ea0011fc70f27e0dd68f5b31f2e7a0
Nayeli Hamzaoui <hamzaoui.sbn@gmail.com>
2018-08-15 09:17:51 +0200

    methods ok except destroy

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Task.create(title: 'Code', details: 'In Ruby')
+Task.create(title: 'Courses', details: 'fruits et légumes')
+Task.create(title: 'Sport', details: "courir au parc de la tête d'or")
